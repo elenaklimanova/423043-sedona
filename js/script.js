fb37c35fdec493631edd44eee2527cb60bbe8d29
elenaklimanova <e.klimanova1@mail.ru>
2017-05-12 22:51:34 +0400

    правки

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ var button = document.querySelector(".offer-btn");
 var popup = document.querySelector(".hotel-search");
 
 button.addEventListener("click", function (event) {
-event.preventDefault();
-if (popup.classList.contains("hotel-show")){
-popup.classList.remove("hotel-show");
-} else {
-popup.classList.add("hotel-show");
-}
+  event.preventDefault();
+  if (popup.classList.contains("hotel-show")) {
+    popup.classList.remove("hotel-show");
+  } else {
+    popup.classList.add("hotel-show");
+  }
 });
